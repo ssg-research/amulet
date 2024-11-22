b20f1831030743474b942cea1581f679786dbd04
Asim Waheed <asim.waheed29@gmail.com>
2024-11-21 23:15:47 -0500

    Fix type errors

--- amulet/datasets/__image_datasets.py
+++ amulet/datasets/__image_datasets.py
@@ -7,12 +7,13 @@ from pathlib import Path
 
 import torchvision.transforms as transforms
 import pandas as pd
-import numpy as np 
+import numpy as np
 import torch
 from torchvision import datasets
 from torch.utils.data import TensorDataset
 from .__data import AmuletDataset
 from sklearn.model_selection import train_test_split
+
 
 def load_cifar10(
     path: str | Path = Path("./data/cifar10"),
@@ -149,10 +150,11 @@ def load_fmnist(
         train_set=train_set, test_set=test_set, num_features=28 * 28, num_classes=10
     )
 
+
 def load_celeba(
     path: str | Path = Path("./data/celeba"),
     random_seed: int = 0,
-    test_size: float = 0.5, 
+    test_size: float = 0.5,
 ) -> AmuletDataset:
     """
     Loads the CelebA released by https://mmlab.ie.cuhk.edu.hk/projects/CelebA.html.
@@ -188,40 +190,60 @@ def load_celeba(
             z_test: :class:`~np.ndarray`
                 Sensitive attribute labels for the test data.
     """
-    #TODO: Write code to download the dataset from Google Drive.
+    # TODO: Write code to download the dataset from Google Drive.
 
     if isinstance(path, str):
         path = Path(path)
 
-    df = pd.read_csv(path / 'celeba.csv', na_values="NA", index_col=None, sep=",", header=0)
-    df['pixels']= df['pixels'].apply(lambda x:  np.array(x.split(), dtype="float32"))
-    df['pixels'] = df['pixels'].apply(lambda x: x/255)
-    df['pixels'] = df['pixels'].apply(lambda x:  np.reshape(x, (3, 48,48)))
+    df = pd.read_csv(
+        path / "celeba.csv", na_values="NA", index_col=None, sep=",", header=0
+    )
+    df["pixels"] = df["pixels"].apply(lambda x: np.array(x.split(), dtype="float32"))
+    df["pixels"] = df["pixels"].apply(lambda x: x / 255)
+    df["pixels"] = df["pixels"].apply(lambda x: np.reshape(x, (3, 48, 48)))
 
-    images = df['pixels'].to_frame()
-    
+    images = df["pixels"].to_frame()
+
     images_np = np.stack(images["pixels"].to_list())
-    attributes = df[['Smiling', 'Male']]
+    target_attribute: str = "Smiling"
+    sensitive_attributes: list[str] = ["Male"]
+    attributes = df[["Smiling"] + sensitive_attributes]
 
-    images_train, images_test, attributes_train, attributes_test = train_test_split(images_np, attributes, test_size = test_size, stratify = attributes, random_state = random_seed)
+    images_train, images_test, attributes_train, attributes_test = train_test_split(
+        images_np,
+        attributes,
+        test_size=test_size,
+        stratify=attributes,
+        random_state=random_seed,
+    )
 
-    target_train = attributes_train['Smiling'].to_numpy()
-    target_test = attributes_test['Smiling'].to_numpy()
-    sensitive_train = attributes_train[['Male']].to_numpy()
-    sensitive_test = attributes_test[['Male']].to_numpy()
+    # For type setting, the train_test_split function messes with the data type
+    attributes_train = pd.DataFrame(attributes_train)
+    attributes_test = pd.DataFrame(attributes_test)
 
-    train_set = TensorDataset(torch.from_numpy(images_train).type(torch.FloatTensor), torch.from_numpy(target_train).type(torch.LongTensor))
-    test_set = TensorDataset(torch.from_numpy(images_test).type(torch.FloatTensor), torch.from_numpy(target_test).type(torch.LongTensor))
+    target_train = attributes_train[target_attribute].to_numpy()
+    target_test = attributes_test[target_attribute].to_numpy()
+    sensitive_train = attributes_train[sensitive_attributes].to_numpy()
+    sensitive_test = attributes_test[sensitive_attributes].to_numpy()
+
+    train_set = TensorDataset(
+        torch.from_numpy(images_train).type(torch.float32),
+        torch.from_numpy(target_train).type(torch.long),
+    )
+    test_set = TensorDataset(
+        torch.from_numpy(images_test).type(torch.float32),
+        torch.from_numpy(target_test).type(torch.long),
+    )
 
     return AmuletDataset(
-        train_set = train_set,
-        test_set = test_set,
-        num_features = 48*48,
-        num_classes = 2,
-        x_train = images_train,
-        x_test = images_test,
-        y_train = target_train,
-        y_test = target_test,
-        z_train = sensitive_train,
-        z_test = sensitive_test,
+        train_set=train_set,
+        test_set=test_set,
+        num_features=48 * 48,
+        num_classes=2,
+        x_train=np.array(images_train),
+        x_test=np.array(images_test),
+        y_train=target_train,
+        y_test=target_test,
+        z_train=sensitive_train,
+        z_test=sensitive_test,
     )
