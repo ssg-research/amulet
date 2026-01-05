1e9a22d9610991601b6290209e6f274f642864b2
Asim Waheed <a7waheed@uwaterloo.ca>
2026-01-05 19:25:06 +0000

    Update models; include simple CNN, add configuration for resnet and linear_net

--- amulet/models/__init__.py
+++ amulet/models/__init__.py
@@ -4,6 +4,7 @@ The module mlconf.models includes utilities to build sample models.
 
 from .vgg import VGG
 from .linear_net import LinearNet
-from .resnet import ResNet18
+from .resnet import ResNet
+from .cnn import SimpleCNN
 
-__all__ = ["VGG", "LinearNet", "ResNet18"]
+__all__ = ["VGG", "LinearNet", "ResNet", "SimpleCNN"]
